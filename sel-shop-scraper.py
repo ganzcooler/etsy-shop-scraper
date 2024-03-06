ef7894497b091a835018b67a176ded6d383ed0b5
dano <ganzcooler@protonmail.com>
2024-03-06 10:27:17 +0100

    Get all items on all pages

--- sel-shop-scraper.py
+++ sel-shop-scraper.py
@@ -1,20 +1,44 @@
 from selenium import webdriver
+import time
 
-with webdriver.Firefox() as driver:
+# Erstelle eine Option, um die bevorzugte Sprache einzustellen
+options = webdriver.FirefoxOptions()
+options.set_preference('intl.accept_languages', 'de-DE')
+
+with webdriver.Firefox(options=options) as driver:
 
     # Webseite aufrufen
     driver.get('https://www.etsy.com/de/shop/BullwoodDE/sold')
 
-    # cookiemeldung akzeptieren
+    # Cookiemeldung akzeptieren und 2 sek warten
     btn_akzeptieren = driver.find_element("xpath", "/html/body/div[8]/div[2]/div/div[2]/div/div[2]/div[2]/button")
     btn_akzeptieren.click()
+    time.sleep(2)
 
-    # Alle h3-Tags abrufen, deren id-Attribut das Wort "listing" enthält
-    listing_h3_tags = driver.find_elements("xpath", "//h3[contains(@id, 'listing')]")
+    # Öffnen der Datei zum Schreiben
+    with open('bullwood.txt', 'w', encoding='utf-8') as file:
 
-    # Ausgabe der Texte der gefundenen h3-Tags
-    for tag in listing_h3_tags:
-        print(tag.text)
+        # Alle h3-Tags abrufen, deren id-Attribut das Wort "listing" enthält
+        for i in range(1, 2):
+            # Url anpassen
+            url = f'https://www.etsy.com/de/shop/BullwoodDE/sold?ref=pagination&page={i}'
 
-# Webdriver schließen
-driver.quit()
+            try:
+                # Ausgabe in Konsole über Status
+                print(f"Rufe Seite {i} ab...")
+
+                # Webseite aufrufen
+                driver.get(url)
+
+                # Alle h3-Tags abrufen
+                listing_h3_tags = driver.find_elements("xpath", "//h3[contains(@id, 'listing')]")
+
+                # Schreiben der Texte der gefundenen h3-Tags in die Datei
+                for tag in listing_h3_tags:
+                    file.write(tag.text + '\n')
+
+            except Exception as e:
+                # Schreiben der Fehlermeldung in die Datei
+                file.write(f'Fehler beim Abrufen von {url}: {str(e)}\n')
+
+print("Done.")
